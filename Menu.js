4b74ee20044b55c80227e0a8e302961acda4caf1
Jadson Rafael <35284924+rafael10elt@users.noreply.github.com>
2018-01-19 19:16:34 -0500

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,13 @@
+//alert("I am an alert box!");
+
+var BtnScreenManager = document.getElementById("UserManager");
+
+
+
+function UserManager_Click(){
+  // BtnScreenManager.click();
+  window.location.href='UserManagerScreen.html';
+}
+ 
+
+    
